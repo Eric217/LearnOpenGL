b83ed8011b49ba7b5f00db23ce64be3e5fdff2d8
Eric <erisl@icloud.com>
2022-09-21 18:35:44 +0800

    搭建工程

---
+++ src/01/main01.c
@@ -0,0 +1,24 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+int main() {
+    
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    
+    GLFWwindow *window = glfwCreateWindow(800, 600, "01", 0, 0);
+    if (!window) {
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        return -1;
+    }
+    
+    
+    return 0;
+}
